6aefcfb3fce88122af2a3d3a4e234e8e85ce2534
shantanu250907 <shantanusonawane2007@gmail.com>
2025-07-30 00:54:04 +0530

    Delete projects directory

--- projects/src/Jspractice.js
+++
@@ -1,8 +0,0 @@
-const games=["Cricket","Hockey","Football","Volleyball","Tennis","Golf"]
-
-      games.map((item)=>{
-        console.log(item);
-      })
-        console.log(games.map((item)=>{
-            return name
-      }));
